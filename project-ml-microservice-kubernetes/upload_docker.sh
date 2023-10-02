6c77d0df4113e4a02cfa8577a569e5a865be9408
EC2 Default User <ec2-user@ip-172-31-2-29.ec2.internal>
2023-10-02 18:00:16 +0000

    microservices

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=rajfast/udacityrajms
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker image tag udacityrajms $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
